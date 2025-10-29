cc3902b0b6e7e10cd53e0dff1c81f74d7aaa3c24
antagonizzzt <122402982+antagonizzzt@users.noreply.github.com>
2025-10-29 20:37:25 +0530

    Add files via upload

---
+++ lib/thread.c
@@ -0,0 +1,22 @@
+//
+// SPDX-FileCopyrightText: 2025 Roger Ortiz <me@r0rt1z2.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <arch/arm.h>
+#include <arch/ops.h>
+#include <lib/common.h>
+#include <lib/debug.h>
+// leaving the above headers for future updates
+
+#include <lib/thread.h>
+
+thread_t *thread_create(const char *name, thread_start_routine entry, void *arg, int priority, size_t stack_size) {
+    return ((thread_t *(*)(const char *, thread_start_routine, void *, int, size_t))
+            (CONFIG_THREAD_CREATE_ADDRESS | 1))(name, entry, arg, priority, stack_size);
+}
+
+int thread_resume(thread_t *t) {
+    return ((int (*)(thread_t *))
+            (CONFIG_THREAD_RESUME_ADDRESS | 1))(t);
+}
